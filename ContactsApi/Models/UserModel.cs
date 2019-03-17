b038f61ebb2e82cc31e16288a13e339c20b520cc
Ahmad Ayyaz <ahmad.ayyaz1993@gmail.com>
2019-03-17 16:19:36 +0500

    * UserPrefs.xml: token based authentication
    * appsettings.json:
    * UserModel.cs:
    * LoginController.cs:
    * ContactsApi.dll:
    * ContactsApi.pdb:
    * ContactsApi.dll:
    * ContactsApi.pdb:
    * ContactsApi.csproj.CoreCompileInputs.cache:

---
+++ ContactsApi/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ContactsApi.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+      
+    }
+}
