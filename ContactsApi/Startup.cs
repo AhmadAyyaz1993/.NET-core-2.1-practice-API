7f66c9dea64066ce059ed4260c9b748bebf3e122
Ahmad Ayyaz <ahmad.ayyaz1993@gmail.com>
2019-03-17 15:59:01 +0500

    * Startup.cs: * appsettings.json:
    * UserPrefs.xml: added configs for jwt token based auth

--- ContactsApi/Startup.cs
+++ ContactsApi/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ContactsApi.Managers;
 using ContactsApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ContactsApi
 {
@@ -27,6 +30,25 @@ namespace ContactsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)  
+            .AddJwtBearer(options =>  
+            {  
+                options.TokenValidationParameters = new TokenValidationParameters  
+                {  
+                    ValidateIssuer = true,  
+                    ValidateAudience = true,  
+                    ValidateLifetime = true,  
+                    ValidateIssuerSigningKey = true,  
+                    ValidIssuer = Configuration["Jwt:Issuer"],  
+                    ValidAudience = Configuration["Jwt:Issuer"],  
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))  
+                };  
+            }); 
+
+
+
             services.AddDbContext<DatabaseContext>(opt =>
                 opt.UseInMemoryDatabase("ContactsList"));
             services.AddScoped(typeof(IDataRepository<>), typeof(ContactManager<>));
@@ -41,7 +63,7 @@ namespace ContactsApi
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
